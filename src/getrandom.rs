04fa5be08571c4f905103cc59c3a4ffaa2543764
famouscat <onismaa@gmail.com>
2023-08-31 12:31:09 +0800

    chore: add getrandom for icp feature

---
+++ src/getrandom.rs
@@ -0,0 +1,9 @@
+/// A getrandom implementation that always fails
+pub fn task(buf: &mut [u8]) -> std::result::Result<(), getrandom::Error> {
+    let time = ic_cdk::api::time();
+    let bytes = time.to_le_bytes();
+    buf.copy_from_slice(&[bytes, bytes].concat());
+    Ok(())
+}
+
+getrandom::register_custom_getrandom!(task);
