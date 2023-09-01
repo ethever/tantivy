8485b68d4dafeafa63e3f1f33189a04ddb3512ae
famouscat <onismaa@gmail.com>
2023-09-01 23:13:19 +0800

    chore: enable fast-rng in uuid dep

--- src/getrandom.rs
+++ src/getrandom.rs
@@ -2,7 +2,7 @@
 pub fn task(buf: &mut [u8]) -> std::result::Result<(), getrandom::Error> {
     let time = ic_cdk::api::time();
     let bytes = time.to_le_bytes();
-    buf.copy_from_slice(&[bytes, bytes].concat());
+    buf.copy_from_slice(&[bytes, bytes, bytes, bytes].concat());
     Ok(())
 }
 
